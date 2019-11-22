43ab34bbf04c317c92f6d570d2c13ecb29b5a41f
Aleksandr Mezin <mezin.alexander@gmail.com>
2019-11-22 16:06:31 +0600

    Add powerlevel9k

--- zshrc.d/40prompt.zsh
+++ zshrc.d/40prompt.zsh
@@ -1,43 +1,51 @@
+fpath+=("${ZDOTDIR}/powerlevel9k")
+
 autoload -Uz promptinit
 promptinit
 
-export POWERLINE_CONFIG_PATHS="${ZDOTDIR}/powerline/powerline/config_files:${ZDOTDIR}/powerline-config"
+if true; then
+    prompt powerlevel9k
 
-"${ZDOTDIR}/bin/powerline-daemon" -q
+    POWERLEVEL9K_LEFT_SEGMENT_SEPARATOR=$'\u258C'
+    POWERLEVEL9K_RIGHT_SEGMENT_SEPARATOR=$'\u2590'
+else
+    export POWERLINE_CONFIG_PATHS="${ZDOTDIR}/powerline/powerline/config_files:${ZDOTDIR}/powerline-config"
 
-source "${ZDOTDIR}/powerline/powerline/bindings/zsh/powerline.zsh"
+    "${ZDOTDIR}/bin/powerline-daemon" -q
+    source "${ZDOTDIR}/powerline/powerline/bindings/zsh/powerline.zsh"
 
-zmodload zsh/datetime
-zmodload zsh/mathfunc
+    zmodload zsh/datetime
+    zmodload zsh/mathfunc
 
-_last_command_duration_preexec() {
-    _last_command_duration_start=${_last_command_duration_start:-$EPOCHREALTIME}
-}
+    _last_command_duration_preexec() {
+        _last_command_duration_start=${_last_command_duration_start:-$EPOCHREALTIME}
+    }
 
-_last_command_duration_precmd() {
-    if [ $_last_command_duration_start ]; then
-        local duration=$(($EPOCHREALTIME - $_last_command_duration_start))
-        unset _last_command_duration_start
+    _last_command_duration_precmd() {
+        if [ $_last_command_duration_start ]; then
+            local duration=$(($EPOCHREALTIME - $_last_command_duration_start))
+            unset _last_command_duration_start
 
-        local humanReadableDuration
+            local humanReadableDuration
 
-        if (( duration > 3600 )); then
-            humanReadableDuration=$(TZ=GMT; strftime '%H:%M:%S' $(( int(rint(duration)) )))
-        elif (( duration > 60 )); then
-            humanReadableDuration=$(TZ=GMT; strftime '%M:%S' $(( int(rint(duration)) )))
-        else
-            typeset -F 2 humanReadableDuration
-            humanReadableDuration=$duration
+            if (( duration > 3600 )); then
+                humanReadableDuration=$(TZ=GMT; strftime '%H:%M:%S' $(( int(rint(duration)) )))
+            elif (( duration > 60 )); then
+                humanReadableDuration=$(TZ=GMT; strftime '%M:%S' $(( int(rint(duration)) )))
+            else
+                typeset -F 2 humanReadableDuration
+                humanReadableDuration=$duration
 
-            if (( $humanReadableDuration == 0 )); then
-                unset _LAST_COMMAND_DURATION_HUMAN_READABLE
-                return
+                if (( $humanReadableDuration == 0 )); then
+                    unset _LAST_COMMAND_DURATION_HUMAN_READABLE
+                    return
+                fi
             fi
+
+            export _LAST_COMMAND_DURATION_HUMAN_READABLE="$humanReadableDuration"
         fi
+    }
 
-        export _LAST_COMMAND_DURATION_HUMAN_READABLE="$humanReadableDuration"
-    fi
-}
-
-precmd_functions+=(_last_command_duration_precmd)
-preexec_functions+=(_last_command_duration_preexec)
+    precmd_functions+=(_last_command_duration_precmd)
+    preexec_functions+=(_last_command_duration_preexec)
+fi
