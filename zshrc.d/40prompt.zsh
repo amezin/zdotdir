87ff2b8c4750d4a127e69405a4f51ae2ab676197
Aleksandr Mezin <mezin.alexander@gmail.com>
2019-11-22 17:00:03 +0600

    Tune powerlevel9k more

--- zshrc.d/40prompt.zsh
+++ zshrc.d/40prompt.zsh
@@ -10,9 +10,12 @@ POWERLEVEL9K_EXECUTION_TIME_ICON=$'\u23F1'
 POWERLEVEL9K_COMMAND_EXECUTION_TIME_THRESHOLD=0.01
 POWERLEVEL9K_COMMAND_EXECUTION_TIME_PRECISION=2
 
+POWERLEVEL9K_STATUS_OK=false
+POWERLEVEL9K_TIME_FORMAT="\u231A %D{%H:%M:%S %d.%m.%y}"
+
 VIRTUAL_ENV_DISABLE_PROMPT=1
 
 POWERLEVEL9K_LEFT_PROMPT_ELEMENTS=(context dir vcs pyenv virtualenv)
-POWERLEVEL9K_RIGHT_PROMPT_ELEMENTS=(command_execution_time status root_indicator background_jobs history time)
+POWERLEVEL9K_RIGHT_PROMPT_ELEMENTS=(command_execution_time status root_indicator background_jobs time)
 
 prompt powerlevel9k
