efa5b203019249a33cbeac2b62010b03e98bcfb3
Aleksandr Mezin <mezin.alexander@gmail.com>
2019-11-23 18:32:50 +0600

    Add pyenv-virtualenv

--- zprofile.d/60pyenv.zsh
+++ zprofile.d/60pyenv.zsh
@@ -2,4 +2,5 @@ export PYENV_ROOT="$ZDOTDIR/pyenv"
 
 prepend_path_if_exists "${PYENV_ROOT}/bin"
 prepend_path_if_exists "$ZDOTDIR/pyenv-register/bin"
+prepend_path_if_exists "$ZDOTDIR/pyenv-virtualenv/bin"
 prepend_path_if_exists "$ZDOTDIR/pyenv-virtualenvwrapper/bin"
--- zshrc.d/53pyenv.zsh
+++ zshrc.d/53pyenv.zsh
@@ -1,3 +1,8 @@
-if command -v pyenv 1>/dev/null 2>&1; then
+export PYENV_VIRTUALENV_VERBOSE_ACTIVATE=1
+
+if command -v pyenv &>/dev/null; then
     eval "$(pyenv init -)"
+    if command -v pyenv-virtualenv-init &>/dev/null; then
+        eval "$(pyenv virtualenv-init -)"
+    fi
 fi
