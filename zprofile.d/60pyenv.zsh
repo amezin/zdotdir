af2ae7fd5ebbd63fb43ba2c4303545c2075f9f34
Aleksandr Mezin <mezin.alexander@gmail.com>
2019-12-22 03:46:10 +0600

    Disable pyenv
    It slows down openwrt builds a lot

--- zprofile.d/60pyenv.zsh
+++
@@ -1,6 +0,0 @@
-export PYENV_ROOT="$ZDOTDIR/pyenv"
-
-prepend_path_if_exists "${PYENV_ROOT}/bin"
-prepend_path_if_exists "$ZDOTDIR/pyenv-register/bin"
-prepend_path_if_exists "$ZDOTDIR/pyenv-virtualenv/bin"
-prepend_path_if_exists "$ZDOTDIR/pyenv-virtualenvwrapper/bin"
--- zshrc.d/53pyenv.zsh
+++
@@ -1,8 +0,0 @@
-export PYENV_VIRTUALENV_VERBOSE_ACTIVATE=1
-
-if command -v pyenv &>/dev/null; then
-    eval "$(pyenv init -)"
-    if command -v pyenv-virtualenv-init &>/dev/null; then
-        eval "$(pyenv virtualenv-init -)"
-    fi
-fi
