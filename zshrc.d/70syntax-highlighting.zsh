1ebd05b2302773fcb6d8b65b243230826d16999d
Aleksandr Mezin <mezin.alexander@gmail.com>
2020-01-09 00:28:37 +0600

    Replace zsh-syntax-highlighting with fast-syntax-highlighting

--- zshrc.d/70syntax-highlighting.zsh
+++ zshrc.d/70syntax-highlighting.zsh
@@ -1,1 +1,2 @@
-source "${ZDOTDIR}/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh"
+#source "${ZDOTDIR}/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh"
+source "${ZDOTDIR}/fast-syntax-highlighting/fast-syntax-highlighting.plugin.zsh"
