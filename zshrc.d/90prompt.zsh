d3e20d845da98e1b25cc4e207f22ef83863e8c8a
Aleksandr Mezin <mezin.alexander@gmail.com>
2020-10-23 15:18:59 +0600

    prompt: enable get-revision and check-for-changes for all vcs

--- zshrc.d/90prompt.zsh
+++ zshrc.d/90prompt.zsh
@@ -21,8 +21,8 @@ VIRTUAL_ENV_DISABLE_PROMPT=1
 POWERLEVEL9K_LEFT_PROMPT_ELEMENTS=(context dir vcs pyenv virtualenv)
 POWERLEVEL9K_RIGHT_PROMPT_ELEMENTS=(command_execution_time status root_indicator background_jobs time)
 
-zstyle ':vcs_info:hg:*:*' get-revision yes
-zstyle ':vcs_info:hg:*:*' check-for-changes yes
+zstyle ':vcs_info:*' get-revision yes
+zstyle ':vcs_info:*' check-for-changes yes
 zstyle ':vcs_info:*' find-deepest yes
 
 prompt powerlevel9k
