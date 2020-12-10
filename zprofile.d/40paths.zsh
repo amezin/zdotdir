c7acb8f29a87ae88f6e653021bb241a293421d1b
Aleksandr Mezin <mezin.alexander@gmail.com>
2020-12-10 15:05:07 +0600

    Add depot_tools to PATH

--- zprofile.d/40paths.zsh
+++ zprofile.d/40paths.zsh
@@ -15,6 +15,7 @@ prepend_path_if_exists /Applications/CMake.app/Contents/bin
 prepend_path_if_exists /home/linuxbrew/.linuxbrew/bin
 prepend_path_if_exists "$HOME/Library/Android/sdk/platform-tools"
 prepend_path_if_exists "/Applications/Wine Devel.app/Contents/Resources/wine/bin"
+prepend_path_if_exists "$HOME/depot_tools"
 
 if command -v ruby >/dev/null
 then
