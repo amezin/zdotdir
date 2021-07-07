0c38e84e4c62144360f7980358169860f687b8c0
Aleksandr Mezin <mezin.alexander@gmail.com>
2021-07-07 09:35:06 +0600

    virtualenv auto-activation based on .virtualenvs/*/.project

--- zshrc.d/51virtualenvwrapper.zsh
+++ zshrc.d/51virtualenvwrapper.zsh
@@ -2,21 +2,25 @@ if (( $+commands[virtualenvwrapper_lazy.sh] )); then
   . $commands[virtualenvwrapper_lazy.sh]
 
   function workon_cwd {
-    local PROJECT_ROOT="${PWD:A}"
-    while [[ "$PROJECT_ROOT" != "/" && ! -e "$PROJECT_ROOT/.venv" ]]; do
-      PROJECT_ROOT="${PROJECT_ROOT:h}"
+    local PROJECT_FILE
+    local PROJECT_DIR
+    local PROJECT_VIRTUAL_ENV
+
+    for PROJECT_FILE in "${WORKON_HOME:P}"/*/.project; do
+      local TRY_PROJECT_DIR="$(cat "$PROJECT_FILE")"
+      TRY_PROJECT_DIR="${TRY_PROJECT_DIR:P}"
+      if [[ "${PWD:P}/" == "$TRY_PROJECT_DIR"/* ]]; then
+        if (( ${#TRY_PROJECT_DIR} > ${#PROJECT_DIR} )); then
+          PROJECT_DIR="$TRY_PROJECT_DIR"
+          PROJECT_VIRTUAL_ENV="${PROJECT_FILE:h}"
+        fi
+      fi
     done
-    if [[ "$PROJECT_ROOT" == "/" ]]; then
-      PROJECT_ROOT="."
-    fi
 
     # Check for virtualenv name override
-    if [[ -f "$PROJECT_ROOT/.venv" ]]; then
-      local PROJECT_VIRTUAL_ENV_NAME="$(cat "$PROJECT_ROOT/.venv")"
-      local PROJECT_VIRTUAL_ENV="$WORKON_HOME/$PROJECT_VIRTUAL_ENV_NAME"
-      PROJECT_VIRTUAL_ENV="${PROJECT_VIRTUAL_ENV:P}"
+    if [[ -n "$PROJECT_VIRTUAL_ENV" ]]; then
       if [[ "$PROJECT_VIRTUAL_ENV" != "${VIRTUAL_ENV:P}" ]] && [[ "$PROJECT_VIRTUAL_ENV" != "${AUTO_ACTIVATED_VIRTUAL_ENV:P}" ]]; then
-        workon -n "$PROJECT_VIRTUAL_ENV_NAME" && AUTO_ACTIVATED_VIRTUAL_ENV="$VIRTUAL_ENV"
+        workon -n "${PROJECT_VIRTUAL_ENV:t}" && AUTO_ACTIVATED_VIRTUAL_ENV="$VIRTUAL_ENV"
       fi
     elif [[ -n "$VIRTUAL_ENV" ]] && [[ -d "$VIRTUAL_ENV" ]]; then
       if [[ "$AUTO_ACTIVATED_VIRTUAL_ENV" == "$VIRTUAL_ENV" ]]; then
