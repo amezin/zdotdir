bdc22ef32112f88eb40b1256910a56ad1219899c
Aleksandr Mezin <mezin.alexander@gmail.com>
2022-06-26 18:56:28 +0600

    zshrc.d/prompt: set nix icon based on POWERLEVEL9K_MODE

--- zshrc.d/90prompt.zsh
+++ zshrc.d/90prompt.zsh
@@ -6,10 +6,8 @@ promptinit
 if [[ "$LC_TERMINAL" == "iTerm2" ]]; then
     POWERLEVEL9K_EXECUTION_TIME_ICON=$'\u231B'
     POWERLEVEL9K_TIME_ICON=$'\u231A'
-    POWERLEVEL9K_CUSTOM_NIX_SHELL_ICON='nix'
 elif [[ "$TERM" != (dumb|linux|*bsd*|eterm*) ]]; then
     POWERLEVEL9K_MODE=nerdfont-fontconfig
-    POWERLEVEL9K_CUSTOM_NIX_SHELL_ICON=$'\uF313'
 fi
 
 POWERLEVEL9K_LEFT_SEGMENT_SEPARATOR=
@@ -27,6 +25,15 @@ POWERLEVEL9K_STATUS_OK=false
 VIRTUAL_ENV_DISABLE_PROMPT=1
 
 POWERLEVEL9K_CUSTOM_NIX_SHELL='echo -n ${(M)IN_NIX_SHELL:#(pure|impure)}'
+
+case $POWERLEVEL9K_MODE in
+    'nerdfont-complete'|'nerdfont-fontconfig')
+        POWERLEVEL9K_CUSTOM_NIX_SHELL_ICON=$'\uF313'
+    ;;
+    *)
+        POWERLEVEL9K_CUSTOM_NIX_SHELL_ICON='nix'
+    ;;
+esac
 
 POWERLEVEL9K_LEFT_PROMPT_ELEMENTS=(context dir vcs pyenv virtualenv custom_nix_shell)
 POWERLEVEL9K_RIGHT_PROMPT_ELEMENTS=(command_execution_time status root_indicator background_jobs time)
