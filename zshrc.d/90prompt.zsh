9c9601d59d846ddff2a8a2d90d8bb96b0e108b95
Aleksandr Mezin <mezin.alexander@gmail.com>
2022-09-03 04:31:32 +0600

    prompt: match Nerd Font Mono

--- zshrc.d/90prompt.zsh
+++ zshrc.d/90prompt.zsh
@@ -6,7 +6,7 @@ promptinit
 if [[ -z "$POWERLEVEL9K_MODE" ]]; then
     if [[ -n "$DESKTOP_SESSION" || -n "$XDG_DESKTOP_SESSION" || -n "$XDG_CURRENT_DESKTOP" ]]; then
         if command -V fc-match >/dev/null; then
-            if [[ "$(fc-match -f '%{family[0]}' ':charset=f489')" == "Symbols Nerd Font" ]]; then
+            if [[ "$(fc-match -f '%{family[0]}' ':charset=f489')" == "Symbols Nerd Font"* ]]; then
                 POWERLEVEL9K_MODE=nerdfont-fontconfig
             fi
         fi
