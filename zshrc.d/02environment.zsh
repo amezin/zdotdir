e6802f9c199fc1d5ae31f991c3f7e84e58b83d89
Aleksandr Mezin <mezin.alexander@gmail.com>
2025-05-11 13:28:44 +0300

    Enable source-highlight highlighting in less

--- zshrc.d/02environment.zsh
+++ zshrc.d/02environment.zsh
@@ -61,6 +61,6 @@ export LESS='-F -g -i -M -R -S -w -X -z-4'
 
 # Set the Less input preprocessor.
 # Try both `lesspipe` and `lesspipe.sh` as either might exist on a system.
-if (( $#commands[(i)lesspipe(|.sh)] )); then
-  export LESSOPEN="| /usr/bin/env $commands[(i)lesspipe(|.sh)] %s 2>&-"
+if (( $#commands[(i)(lesspipe(|.sh)|src-hilite-lesspipe.sh)] )); then
+  export LESSOPEN="| /usr/bin/env $commands[(i)(lesspipe(|.sh)|src-hilite-lesspipe.sh)] %s 2>&-"
 fi
